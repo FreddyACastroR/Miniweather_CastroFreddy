457d0dc90b0362ef9863838050c0cca21f5d2e06
Freddy Castro <freddy.castro252191@potros.itson.edu.mx>
2026-09-05 20:06:43 -0700

    hecho en clase

--- app/src/main/java/castrofreddy/miniweather_castrof/CityActivity.kt
+++ app/src/main/java/castrofreddy/miniweather_castrof/CityActivity.kt
@@ -1,12 +1,22 @@
 package castrofreddy.miniweather_castrof
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import castrofreddy.miniweather_castrof.utilities.WeatherService
 
 class CityActivity : AppCompatActivity() {
+
+    var citySelected: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,39 @@ class CityActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val service: WeatherService = WeatherService(this)
+        val citySelector: Spinner = findViewById<Spinner>(R.id.city_selector)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
+            service.getCities()
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        citySelector.adapter = adapter
+
+        citySelector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                citySelected = parent!!.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        val buttonNext = findViewById<Button>(R.id.btn_save_city)
+        buttonNext.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra("city", citySelected)
+            }
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/castrofreddy/miniweather_castrof/domain/Weather.kt
@@ -0,0 +1,3 @@
+package castrofreddy.miniweather_castrof.domain
+
+data class Weather(val temperature: Int, val weather: String)
